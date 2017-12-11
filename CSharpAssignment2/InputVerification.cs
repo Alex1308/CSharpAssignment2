5448421272ef1ac67e2fd4ae695c97841e01c4e8
Ivanna <alexander.gehling@gmail.com>
2017-12-11 23:53:00 +0100

    Added classes to handle verification and persistence. Created basic GUI.

---
+++ CSharpAssignment2/InputVerification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAssignment2
+{
+    class InputVerification
+    {
+
+        private PersistenceManager pManager;
+
+        public InputVerification()
+        {
+            pManager = new PersistenceManager();
+        }
+
+        public bool VerifyInput(String[] data)
+        {
+            
+            return true;
+        }
+    }
+}
---
+++ CSharpAssignment2/PersistenceManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace CSharpAssignment2
+{
+    class PersistenceManager
+    {
+
+
+
+        public PersistenceManager()
+        {
+        }
+
+        private async void CreateFolder()
+        {
+            var localFolder = ApplicationData.Current.LocalFolder;
+            var pFolder = await localFolder.CreateFolderAsync("pFolder", CreationCollisionOption.OpenIfExists);
+        }
+
+        public bool AddToFolder()
+        {
+
+            return true;
+        }
+
+    }
+}
